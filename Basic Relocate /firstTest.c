176749253e690f9e9c55f25a9db27a79c02be985
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-12-03 14:57:48 +0100

    first test to compile

---
+++ Basic Relocate /firstTest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Please Enter a number : ");
+    scanf("%d", &num);
+
+    printf("You entered %d", num);
+    getchar();
+    return 0;
+}
