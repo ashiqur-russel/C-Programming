59b40f3b1dcc385db1e196f04c11fa8ad126df59
Mohammad Ashiqur Rahman <ashiqurrussel2@gmail.com>
2020-12-11 01:37:28 +0100

    fahrenheit to celcius convert in C

---
+++ Variable & Arithmetic Expression/Fahreneheit-Celcius.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int fahr, celcius, lower, upper, step;
+
+    lower = 0;   /* lower limit of temp table*/
+    upper = 300; /* upper limit */
+
+    step = 20;
+
+    fahr = lower;
+
+    while (fahr <= upper)
+    {
+        celcius = 5 * (fahr - 32) / 9;
+
+        printf("%d \t %d \n", fahr, celcius);
+        fahr += step;
+    }
+}
