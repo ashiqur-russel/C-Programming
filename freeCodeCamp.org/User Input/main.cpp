390258402130ffa475e1140fc558e9e33bf9f875
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-29 00:36:04 +0100

    user input

---
+++ freeCodeCamp.org/User Input/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "you are " << age << " years old" << endl;
+
+    string name;
+    cout << "Enter your name : " << endl;
+    cin >> name;
+    cout << "Hello" << name << endl;
+
+    return 0;
+}
