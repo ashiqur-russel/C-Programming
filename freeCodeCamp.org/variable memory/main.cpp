6eb01627314e4a31e08b3138671ee647a3a51fe6
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-28 22:22:01 +0100

    variable in c++

---
+++ freeCodeCamp.org/variable memory/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    string characterName = "John";
+    int characterAge = 20;
+    cout << "His name is " << characterName << " and He is " << characterAge << endl;
+    return 0;
+}
