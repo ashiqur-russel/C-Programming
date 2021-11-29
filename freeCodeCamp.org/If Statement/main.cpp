15789e7e60dff76d51e4fb6c1350f58a8303ff72
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-29 01:48:02 +0100

    if statement

---
+++ freeCodeCamp.org/If Statement/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    bool isMale = true;
+    bool isTall = true;
+
+    if (isMale && isTall)
+    {
+        cout << "you are a male" << endl;
+    }
+    else
+    {
+        cout << "you are a female" << endl;
+    }
+    return 0;
+}
