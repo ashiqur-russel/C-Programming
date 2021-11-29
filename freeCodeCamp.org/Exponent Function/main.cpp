88aa69aadb3aafa007b9ee2049d5520462f0e4dc
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-30 00:00:26 +0100

    Exponent function

--- freeCodeCamp.org/Exponent Function/main.cpp
+++ freeCodeCamp.org/Exponent Function/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int power(int baseNum, int powNum)
+{
+    int result = 1;
+
+    for (int i = 0; i < powNum; i++)
+    {
+        result = result * baseNum;
+    }
+
+    return result;
+}
 int main()
 
 {
@@ -10,5 +21,7 @@ int main()
     {
         cout << nums[i] << endl;
     }
+
+    cout << "Power : " << power(4, 2) << endl;
     return 0;
 }
